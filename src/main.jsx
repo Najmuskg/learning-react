cac6cd21002057e03b35fcfc4f346547bd304a7a
Sakib <najmuskg@gmail.com>
2025-01-30 11:46:59 +0600

    components path modified

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./output.css";
 import App from "./App.jsx";
-import Navbar from "../../Components/Navbar.jsx";
-import Footer from "../../Components/Footer.jsx";
+// import Navbar from "../";
+// import Footer from "../../Components/Footer.jsx";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
