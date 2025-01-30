f425f001fa06d95d5e72a733d83563646cfeac5f
Sakib <najmuskg@gmail.com>
2025-01-30 11:10:57 +0600

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="text-red-300 underline text-3xl font-bold"> Hello world! </h1>
+
+      <h2 className="text-yellow-200">This Vite Project</h2>
+    </>
+  );
+}
+
+export default App;
