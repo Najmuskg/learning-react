9a71cf02a6f98b24d0392080e246871bb3fcd4a6
Sakib <najmuskg@gmail.com>
2025-01-30 11:41:11 +0600

    testing two component's have been added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './output.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./output.css";
+import App from "./App.jsx";
+import Navbar from "../../Components/Navbar.jsx";
+import Footer from "../../Components/Footer.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Navbar />
+
     <App />
-  </StrictMode>,
-)
+
+    <Footer />
+  </StrictMode>
+);
